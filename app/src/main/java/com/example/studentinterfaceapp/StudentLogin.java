e9466dfc8fa87cc33814f6b147b47a5b70f8e939
Ankita <ankitrajkaur@gmail.com>
2019-12-07 01:44:32 -0500

    Student login file

--- app/src/main/java/com/example/studentinterfaceapp/StudentLogin.java
+++ app/src/main/java/com/example/studentinterfaceapp/StudentLogin.java
@@ -1,6 +1,27 @@
 package com.example.studentinterfaceapp;
 
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class StudentLogin extends AppCompatActivity {
+
+    EditText editText;
+    EditText editText2;
+    TextView textView;
+    Button button;
+
+    protected void onCreate (Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+
+        editText = (EditText) findViewById(R.id.editText);
+        editText2 = (EditText) findViewById(R.id.editText2);
+        textView = (TextView) findViewById(R.id.textView);
+        button = (Button) findViewById(R.id.button);
+    }
+
 }
